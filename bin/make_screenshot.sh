bc3126ea7f75b18048a01f2209082a0159f6dc25
DevAlone <dev@d3d.info>
2019-10-02 15:29:44 +0300

    copy the latest screenshot to clipboard

--- bin/make_screenshot.sh
+++ bin/make_screenshot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-filename=/home/user/screenshots/%Y-%m-%d-%T-screenshot.png
+filename=/home/user/screenshots/$(date +%Y-%m-%d-%T)-screenshot.png
 
 sleep 0.3s;
 
@@ -17,4 +17,3 @@ case $1 in
 esac
 
 xclip -selection clipboard -t image/png -i $filename
-# xclip $filename
