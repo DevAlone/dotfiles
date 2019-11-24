def73e6e7e178cd8e1dcfe226afd683df9e3a5e9
DevAlone <dev@d3d.info>
2019-11-24 12:31:57 +0000

    add fedora support

--- .config/nvim/install_deps.sh
+++ .config/nvim/install_deps.sh
@@ -30,13 +30,20 @@ else
 	VER=$(uname -r)
 fi
 
+# TODO: install vim-plug
 case "$OS" in
 	"Debian GNU/Linux")
-		apt-get install -y clang-format python-autopep8 flake8 pylint pyflakes bandit flake8 mypy prospector pycodestyle pydocstyle pyflakes pylama pylint vulture
+		apt-get install -y exuberant_ctags clang-format python-autopep8 flake8 pylint pyflakes bandit mypy prospector pycodestyle pydocstyle pyflakes pylama pylint vulture
 		sudo -u user pip3 install --user 'python-language-server[all]' 'pyre-check'
+		sudo -u user pip3 install --user 'neovim'
+		npm install -g js-beautify
+		;;
+	"Fedora")
+		dnf install -y nodejs python3 python-pip python3-devel ctags-etags clang python-autopep8 python3-flake8 pylint pyflakes bandit python3-mypy python3-pycodestyle python3-pydocstyle pyflakes pylint python3-vulture || exit 1
+		sudo -u user pip3 install --user 'python-language-server[all]' 'pyre-check' 'prospector[with_pyroma]' 'neovim' 'pylama' || exit 1
 		npm install -g js-beautify
 		;;
 	*)
-		echo "Your distribution is not supported yet"
+		echo "Your distribution '$OS' is not supported yet"
 		;;
 esac
