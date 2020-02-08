0d11ef256b2a2ea2d99cf0ed637157043944d0d7
user <user@email>
2020-02-08 14:59:16 +0100

    touchpad

--- bin/configure_touchpad.sh
+++ bin/configure_touchpad.sh
@@ -9,6 +9,6 @@ xinput set-prop "Elan Touchpad" "libinput Disable While Typing Enabled" 1
 xinput set-prop "Elan Touchpad" "libinput Scroll Method Enabled" {1,0,0}
 xinput set-prop "Elan Touchpad" "libinput Click Method Enabled" {1,0}
 xinput set-prop "Elan Touchpad" "libinput Middle Emulation Enabled" 0
-xinput set-prop "Elan Touchpad" "libinput Accel Speed" 0.500000
+xinput set-prop "Elan Touchpad" "libinput Accel Speed" 0.300000
 xinput set-prop "Elan Touchpad" "libinput Left Handed Enabled" 1
 xinput set-prop "Elan Touchpad" "libinput Horizontal Scroll Enabled" 1
